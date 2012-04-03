a829c47d837c56935d07a9e132cb0aba1b4dc7ea
Calvin Metcalf <calvin.metcalf@state.ma.us>
2012-04-03 12:52:26 +0000

    custimized

--- js/map.js
+++ js/map.js
@@ -1,6 +1,6 @@
 var L = this.L;
 var m = new L.Map('map');
-var geojsonMarkerOptions = {
+var MarkerOptions = {
     radius: 8,
     fillColor: "#ff7800",
     color: "#000",
@@ -8,19 +8,24 @@ var geojsonMarkerOptions = {
     opacity: 1,
     fillOpacity: 0.8
 };
-var geojson = new L.GeoJSON(null, {
+var WT = new L.GeoJSON(null, {
     pointToLayer: function (latlng) {
-        return new L.CircleMarker(latlng, geojsonMarkerOptions);
+        return new L.CircleMarker(latlng, MarkerOptions);
     }
 });
 
 var mapQuestURL = 'http://otile{s}.mqcdn.com/tiles/1.0.0/osm/{z}/{x}/{y}.png',
 attribution = 'Tiles courtesy MapQuest',
 mQ = new L.TileLayer(mapQuestURL, {attribution: attribution, subdomains: ['1','2','3','4']});
-geojson.on("featureparse", function (e) {
+
+
+WT.on("featureparse", function (e) {
   
         e.layer.bindPopup("located at: " + e.properties.TAXI_STOP + "<br />the service is " + e.properties.SERVICE);
     
 });
-geojson.addGeoJSON(WaterTaxis);
-m.setView(new L.LatLng(42.344335,-71.05442), 13).addLayer(mQ).addLayer(geojson);
+
+
+WT.addGeoJSON(WaterTaxis);
+
+m.setView(new L.LatLng(42.356514,-71.038542), 14).addLayer(mQ).addLayer(WT);
