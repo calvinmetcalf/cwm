400e54b12b0fd18e0002d86fedcc95f30f64382c
Calvin Metcalf <calvin.metcalf@state.ma.us>
2012-04-03 12:03:01 +0000

    custimized

--- js/map.js
+++ js/map.js
@@ -1,9 +1,26 @@
 var L = this.L;
 var m = new L.Map('map');
-var geojson = new L.GeoJSON();
+var geojsonMarkerOptions = {
+    radius: 8,
+    fillColor: "#ff7800",
+    color: "#000",
+    weight: 1,
+    opacity: 1,
+    fillOpacity: 0.8
+};
+var geojson = new L.GeoJSON(null, {
+    pointToLayer: function (latlng) {
+        return new L.CircleMarker(latlng, geojsonMarkerOptions);
+    }
+});
 
 var mapQuestURL = 'http://otile{s}.mqcdn.com/tiles/1.0.0/osm/{z}/{x}/{y}.png',
 attribution = 'Tiles courtesy MapQuest',
 mQ = new L.TileLayer(mapQuestURL, {attribution: attribution, subdomains: ['1','2','3','4']});
+geojson.on("featureparse", function (e) {
+  
+        e.layer.bindPopup("located at: " + e.properties.TAXI_STOP + "<br />the service is " + e.properties.SERVICE);
+    
+});
 geojson.addGeoJSON(WaterTaxis);
 m.setView(new L.LatLng(42.344335,-71.05442), 13).addLayer(mQ).addLayer(geojson);
