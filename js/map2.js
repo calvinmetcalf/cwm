d3e836ef58ab48486cb57d16cf4d276ccd840e33
Calvin Metcalf <calvin.metcalf@state.ma.us>
2012-04-03 15:22:08 +0000

    added liniar one

---
+++ js/map2.js
@@ -0,0 +1,20 @@
+var L = this.L;
+var m = new L.Map('map');
+
+var FR = new L.GeoJSON();
+
+var mapQuestURL = 'http://otile{s}.mqcdn.com/tiles/1.0.0/osm/{z}/{x}/{y}.png',
+attribution = 'Tiles courtesy MapQuest',
+mQ = new L.TileLayer(mapQuestURL, {attribution: attribution, subdomains: ['1','2','3','4']});
+
+FR.on("featureparse", function (e) {
+  
+       e.layer.bindPopup("Type: " + e.properties.TYPE + "<br />  Operator: " + e.properties.OPERATOR + "<br /> Route: " + e.properties.ROUTE);
+    
+});
+
+
+
+FR.addGeoJSON(ferryRoutes);
+
+m.setView(new L.LatLng(42.356514,-71.038542), 8).addLayer(mQ).addLayer(FR);
