149df34e174964872d2a40e7f48cdf2ba0078d8e
Calvin Metcalf <calvin.metcalf@state.ma.us>
2012-04-04 12:32:10 +0000

    custimized line styles

--- ferry/map.js
+++ ferry/map.js
@@ -3,6 +3,12 @@ var m = new L.Map('map');
 
 var FR = new L.GeoJSON();
 
+var lineStyle = {
+            "color": "#ff7800",
+            "weight": 3,
+            "opacity": 0.9
+        };
+
 var mapQuestURL = 'http://otile{s}.mqcdn.com/tiles/1.0.0/osm/{z}/{x}/{y}.png',
 attribution = 'Tiles courtesy MapQuest',
 mQ = new L.TileLayer(mapQuestURL, {attribution: attribution, subdomains: ['1','2','3','4']});
@@ -10,7 +16,7 @@ mQ = new L.TileLayer(mapQuestURL, {attribution: attribution, subdomains: ['1','2
 FR.on("featureparse", function (e) {
   
        e.layer.bindPopup("Type: " + e.properties.TYPE + "<br />Operator: " + e.properties.OPERATOR + "<br />Route: " + e.properties.ROUTE + "<br />Route Number: " + e.properties.RTE_NUM + "<br />Service: " + e.properties.SERVICE);
-    
+    e.layer.setStyle(lineStyle);
 });
 
 
