43cf544ba152a9cc747bf8372b46a24ca0a7669b
Calvin Metcalf <calvin.metcalf@state.ma.us>
2012-04-04 12:47:17 +0000

    added seaports

---
+++ seaports/Seaports.js
@@ -0,0 +1,67 @@
+var seaPort = {
+"type": "FeatureCollection",
+"features": [
+{ "type": "Feature", "id": 0, "properties": { "IMS_TERMIN": 29, "TERM_NAME": "Boston Autoport", "FREIGHT": "Yes", "PASSENGER": "No", "NHS": "Yes", "TOWN": "Boston", "SERVICE": "year round", "MPO": "Boston Region" }, "geometry": { "type": "Point", "coordinates": [ -71.053528, 42.382340 ] } }
+,
+{ "type": "Feature", "id": 1, "properties": { "IMS_TERMIN": 57, "TERM_NAME": "Conley Terminal, Port of Boston", "FREIGHT": "Yes", "PASSENGER": "No", "NHS": "Yes", "TOWN": "Boston", "SERVICE": "year round", "MPO": "Boston Region" }, "geometry": { "type": "Point", "coordinates": [ -71.021170, 42.340862 ] } }
+,
+{ "type": "Feature", "id": 2, "properties": { "IMS_TERMIN": 70, "TERM_NAME": "Woods Hole Ferry Terminal", "FREIGHT": "Yes", "PASSENGER": "Yes", "NHS": "Yes", "TOWN": "Falmouth", "SERVICE": "year round", "MPO": "Cape Cod" }, "geometry": { "type": "Point", "coordinates": [ -70.670330, 41.522843 ] } }
+,
+{ "type": "Feature", "id": 3, "properties": { "IMS_TERMIN": 72, "TERM_NAME": "Nantucket Ferry Terminal", "FREIGHT": "Yes", "PASSENGER": "Yes", "NHS": "Yes", "TOWN": "Nantucket", "SERVICE": "year round", "MPO": "Nantucket" }, "geometry": { "type": "Point", "coordinates": [ -70.095451, 41.285977 ] } }
+,
+{ "type": "Feature", "id": 4, "properties": { "IMS_TERMIN": 100, "TERM_NAME": "Hyannis Hi-Line Ferry Terminal", "FREIGHT": "No", "PASSENGER": "Yes", "NHS": "Yes", "TOWN": "Barnstable", "SERVICE": "summer", "MPO": "Cape Cod" }, "geometry": { "type": "Point", "coordinates": [ -70.279619, 41.647851 ] } }
+,
+{ "type": "Feature", "id": 5, "properties": { "IMS_TERMIN": 0, "TERM_NAME": "Pemberton Point Pier", "FREIGHT": "No", "PASSENGER": "Yes", "NHS": "No", "TOWN": "Hull", "SERVICE": "year round", "MPO": "Boston Region" }, "geometry": { "type": "Point", "coordinates": [ -70.919601, 42.303643 ] } }
+,
+{ "type": "Feature", "id": 6, "properties": { "IMS_TERMIN": 0, "TERM_NAME": "Fore River Shipyard", "FREIGHT": "No", "PASSENGER": "Yes", "NHS": "No", "TOWN": "Quincy", "SERVICE": "year round", "MPO": "Boston Region" }, "geometry": { "type": "Point", "coordinates": [ -70.973886, 42.242878 ] } }
+,
+{ "type": "Feature", "id": 7, "properties": { "IMS_TERMIN": 8, "TERM_NAME": "Port of New Bedford", "FREIGHT": "Yes", "PASSENGER": "Yes", "NHS": "Yes", "TOWN": "New Bedford", "SERVICE": "year round", "MPO": "Southeastern Massachusetts" }, "geometry": { "type": "Point", "coordinates": [ -70.921018, 41.634671 ] } }
+,
+{ "type": "Feature", "id": 8, "properties": { "IMS_TERMIN": 22, "TERM_NAME": "Hyannis Steamship Ferry Terminal", "FREIGHT": "Yes", "PASSENGER": "Yes", "NHS": "Yes", "TOWN": "Barnstable", "SERVICE": "year round", "MPO": "Cape Cod" }, "geometry": { "type": "Point", "coordinates": [ -70.277358, 41.651362 ] } }
+,
+{ "type": "Feature", "id": 9, "properties": { "IMS_TERMIN": 55, "TERM_NAME": "Rte 1A Petroleum Terminals, Boston", "FREIGHT": "Yes", "PASSENGER": "No", "NHS": "Yes", "TOWN": "Boston", "SERVICE": "year round", "MPO": "Boston Region" }, "geometry": { "type": "Point", "coordinates": [ -71.010074, 42.396397 ] } }
+,
+{ "type": "Feature", "id": 10, "properties": { "IMS_TERMIN": 56, "TERM_NAME": "Black Falcon Cruise Terminal", "FREIGHT": "No", "PASSENGER": "Yes", "NHS": "Yes", "TOWN": "Boston", "SERVICE": "year round", "MPO": "Boston Region" }, "geometry": { "type": "Point", "coordinates": [ -71.030571, 42.343948 ] } }
+,
+{ "type": "Feature", "id": 11, "properties": { "IMS_TERMIN": 60, "TERM_NAME": "Vineyard Haven Ferry Terminal", "FREIGHT": "Yes", "PASSENGER": "Yes", "NHS": "Yes", "TOWN": "Vineyard Haven", "SERVICE": "year round", "MPO": "Martha's Vineyard" }, "geometry": { "type": "Point", "coordinates": [ -70.600616, 41.455452 ] } }
+,
+{ "type": "Feature", "id": 12, "properties": { "IMS_TERMIN": 73, "TERM_NAME": "Oak Bluffs Ferry Terminal", "FREIGHT": "Yes", "PASSENGER": "Yes", "NHS": "Yes", "TOWN": "Oak Bluffs", "SERVICE": "year round", "MPO": "Martha's Vineyard" }, "geometry": { "type": "Point", "coordinates": [ -70.555552, 41.458111 ] } }
+,
+{ "type": "Feature", "id": 13, "properties": { "IMS_TERMIN": 0, "TERM_NAME": "Port of Gloucester", "FREIGHT": "Yes", "PASSENGER": "No", "NHS": "No", "TOWN": "Gloucester", "SERVICE": "year round", "MPO": "Boston Region" }, "geometry": { "type": "Point", "coordinates": [ -70.652012, 42.614262 ] } }
+,
+{ "type": "Feature", "id": 14, "properties": { "IMS_TERMIN": 0, "TERM_NAME": "Port of Salem", "FREIGHT": "Yes", "PASSENGER": "No", "NHS": "No", "TOWN": "Salem", "SERVICE": "year round", "MPO": "Boston Region" }, "geometry": { "type": "Point", "coordinates": [ -70.875224, 42.522768 ] } }
+,
+{ "type": "Feature", "id": 15, "properties": { "IMS_TERMIN": 0, "TERM_NAME": "Port of Fall River", "FREIGHT": "Yes", "PASSENGER": "No", "NHS": "No", "TOWN": "Fall River", "SERVICE": "year round", "MPO": "Southeastern Massachusetts" }, "geometry": { "type": "Point", "coordinates": [ -71.166665, 41.703685 ] } }
+,
+{ "type": "Feature", "id": 16, "properties": { "IMS_TERMIN": 0, "TERM_NAME": "Hingham Shipyard", "FREIGHT": "No", "PASSENGER": "Yes", "NHS": "No", "TOWN": "Hingham", "SERVICE": "year round", "MPO": "Boston Region" }, "geometry": { "type": "Point", "coordinates": [ -70.919675, 42.252962 ] } }
+,
+{ "type": "Feature", "id": 17, "properties": { "IMS_TERMIN": 0, "TERM_NAME": "Harwich Port Ferry", "FREIGHT": "No", "PASSENGER": "Yes", "NHS": "No", "TOWN": "Harwich", "SERVICE": "summer", "MPO": "Cape Cod" }, "geometry": { "type": "Point", "coordinates": [ -70.065288, 41.665164 ] } }
+,
+{ "type": "Feature", "id": 18, "properties": { "IMS_TERMIN": 0, "TERM_NAME": "Gosnold Ferry Terminal", "FREIGHT": "No", "PASSENGER": "Yes", "NHS": "No", "TOWN": "Gosnold", "SERVICE": "summer", "MPO": "Martha's Vineyard" }, "geometry": { "type": "Point", "coordinates": [ -70.928201, 41.424025 ] } }
+,
+{ "type": "Feature", "id": 19, "properties": { "IMS_TERMIN": 0, "TERM_NAME": "Edgartown Ferry Terminal", "FREIGHT": "No", "PASSENGER": "Yes", "NHS": "No", "TOWN": "Edgartown", "SERVICE": "spring-fall", "MPO": "Martha's Vineyard" }, "geometry": { "type": "Point", "coordinates": [ -70.510389, 41.389452 ] } }
+,
+{ "type": "Feature", "id": 20, "properties": { "IMS_TERMIN": 0, "TERM_NAME": "MacMillan Pier", "FREIGHT": "No", "PASSENGER": "Yes", "NHS": "No", "TOWN": "Provincetown", "SERVICE": "summer", "MPO": "Cape Cod" }, "geometry": { "type": "Point", "coordinates": [ -70.185164, 42.050738 ] } }
+,
+{ "type": "Feature", "id": 21, "properties": { "IMS_TERMIN": 0, "TERM_NAME": "Logan Airport Ferry Terminal", "FREIGHT": "No", "PASSENGER": "Yes", "NHS": "No", "TOWN": "Boston", "SERVICE": "year round", "MPO": "Boston Region" }, "geometry": { "type": "Point", "coordinates": [ -71.027036, 42.359921 ] } }
+,
+{ "type": "Feature", "id": 22, "properties": { "IMS_TERMIN": 0, "TERM_NAME": "Rowe's Wharf", "FREIGHT": "No", "PASSENGER": "Yes", "NHS": "No", "TOWN": "Boston", "SERVICE": "year round", "MPO": "Boston Region" }, "geometry": { "type": "Point", "coordinates": [ -71.050054, 42.356903 ] } }
+,
+{ "type": "Feature", "id": 23, "properties": { "IMS_TERMIN": 0, "TERM_NAME": "Long Wharf", "FREIGHT": "No", "PASSENGER": "Yes", "NHS": "No", "TOWN": "Boston", "SERVICE": "year round", "MPO": "Boston Region" }, "geometry": { "type": "Point", "coordinates": [ -71.050653, 42.359816 ] } }
+,
+{ "type": "Feature", "id": 24, "properties": { "IMS_TERMIN": 0, "TERM_NAME": "Charlestown Navy Yard", "FREIGHT": "No", "PASSENGER": "Yes", "NHS": "No", "TOWN": "Boston", "SERVICE": "year round", "MPO": "Boston Region" }, "geometry": { "type": "Point", "coordinates": [ -71.054763, 42.373106 ] } }
+,
+{ "type": "Feature", "id": 25, "properties": { "IMS_TERMIN": 0, "TERM_NAME": "World Trade Center Ferry Terminal", "FREIGHT": "No", "PASSENGER": "Yes", "NHS": "No", "TOWN": "Boston", "SERVICE": "year round", "MPO": "Boston Region" }, "geometry": { "type": "Point", "coordinates": [ -71.040868, 42.351079 ] } }
+,
+{ "type": "Feature", "id": 26, "properties": { "IMS_TERMIN": 0, "TERM_NAME": "George's Island", "FREIGHT": "No", "PASSENGER": "Yes", "NHS": "No", "TOWN": "Boston", "SERVICE": "summer", "MPO": "Boston Region" }, "geometry": { "type": "Point", "coordinates": [ -70.930288, 42.319452 ] } }
+,
+{ "type": "Feature", "id": 27, "properties": { "IMS_TERMIN": 0, "TERM_NAME": "State Pier", "FREIGHT": "No", "PASSENGER": "Yes", "NHS": "No", "TOWN": "Plymouth", "SERVICE": "summer", "MPO": "Old Colony" }, "geometry": { "type": "Point", "coordinates": [ -70.662470, 41.959680 ] } }
+,
+{ "type": "Feature", "id": 28, "properties": { "IMS_TERMIN": 0, "TERM_NAME": "Falmouth Harbor", "FREIGHT": "No", "PASSENGER": "Yes", "NHS": "No", "TOWN": "Falmouth", "SERVICE": "spring-fall", "MPO": "Cape Cod" }, "geometry": { "type": "Point", "coordinates": [ -70.600356, 41.550760 ] } }
+,
+{ "type": "Feature", "id": 29, "properties": { "IMS_TERMIN": 0, "TERM_NAME": "Falmouth Marine", "FREIGHT": "No", "PASSENGER": "Yes", "NHS": "No", "TOWN": "Falmouth", "SERVICE": "spring-fall", "MPO": "Cape Cod" }, "geometry": { "type": "Point", "coordinates": [ -70.605392, 41.545799 ] } }
+,
+{ "type": "Feature", "id": 30, "properties": { "IMS_TERMIN": 0, "TERM_NAME": "Central Wharf", "FREIGHT": "No", "PASSENGER": "Yes", "NHS": "No", "TOWN": "Boston", "SERVICE": "year round", "MPO": "Boston Region" }, "geometry": { "type": "Point", "coordinates": [ -71.050397, 42.358744 ] } }
+
+]
+}
---
+++ seaports/map.js
@@ -0,0 +1,31 @@
+
+var m = new L.Map('map');
+var MarkerOptions = {
+    radius: 6,
+    fillColor: "#ff7800",
+    color: "#000",
+    weight: 1,
+    opacity: 1,
+    fillOpacity: 1
+};
+var PT = new L.GeoJSON(null, {
+    pointToLayer: function (latlng) {
+        return new L.CircleMarker(latlng, MarkerOptions);
+    }
+});
+
+var mapQuestURL = 'http://otile{s}.mqcdn.com/tiles/1.0.0/osm/{z}/{x}/{y}.png',
+attribution = 'Tiles courtesy MapQuest',
+mQ = new L.TileLayer(mapQuestURL, {attribution: attribution, subdomains: ['1','2','3','4']});
+
+
+PT.on("featureparse", function (e) {
+  
+        e.layer.bindPopup(e.properties.TERM_NAME + "<br />the service is " + e.properties.SERVICE + "<br />located in " + e.properties.TOWN);
+    
+});
+
+
+PT.addGeoJSON(seaPort);
+
+m.setView(new L.LatLng(42.151187,-70.949707), 9).addLayer(mQ).addLayer(PT);
