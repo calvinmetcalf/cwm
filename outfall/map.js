7e4f451c87dafd9291034e02fd9a174b868ef472
Calvin Metcalf <calvin.metcalf@state.ma.us>
2012-04-09 16:08:22 +0000

    added outfall locations

---
+++ outfall/map.js
@@ -0,0 +1,31 @@
+
+var m = new L.Map('map');
+var MarkerOptions = {
+    radius: 6,
+    fillColor: "#ff7800",
+    color: "#000",
+    weight: 1,
+    opacity: 1,
+    fillOpacity: 1
+};
+var PT = new L.GeoJSON(null, {
+    pointToLayer: function (latlng) {
+        return new L.CircleMarker(latlng, MarkerOptions);
+    }
+});
+
+var mapQuestURL = 'http://otile{s}.mqcdn.com/tiles/1.0.0/osm/{z}/{x}/{y}.png',
+attribution = 'Tiles courtesy MapQuest',
+mQ = new L.TileLayer(mapQuestURL, {attribution: attribution, subdomains: ['1','2','3','4']});
+
+
+PT.on("featureparse", function (e) {
+  
+        e.layer.bindPopup("type: " + e.properties.Otype + "<br />Shape: " + e.properties.Oshape + "<br />Rise Diameter: " + e.properties.Rise + "<br />Span Diameter: " + e.properties.Span + "<br />Material: " + e.properties.Material);
+    
+});
+
+
+PT.addGeoJSON(outFall);
+
+m.setView(new L.LatLng(41.914541,-71.592407), 8).addLayer(mQ).addLayer(PT);
